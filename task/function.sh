54b0ec1063555b3d85a23eed93c6b496cbcf918b
cumet04 <medalhkr@gmail.com>
2022-12-03 16:09:36 +0900

    article-step2

--- task/function.sh
+++ task/function.sh
@@ -1,7 +1,21 @@
-function handler () {
-  EVENT_DATA=$1
-  echo "$EVENT_DATA" 1>&2;
-  RESPONSE="Echoing request: '$EVENT_DATA'"
+# AmazonLinux2ベースのイメージでありmysqlパッケージは無い。のでmysqldumpはmariadbのもので代用
+yum install -y gzip awscli mariadb
 
-  echo $RESPONSE
+# オプションが長すぎるので関数にしてわかりやすくする
+function getparam() {
+  aws ssm get-parameter --with-decryption --region ap-northeast-1 --name $1 --query 'Parameter.Value' --output text
+}
+export DBUSER=$(getparam sbox-mysqldump-dbuser)
+export DBHOST=$(getparam sbox-mysqldump-dbhost)
+export DBNAME=$(getparam sbox-mysqldump-dbname)
+export DBPASS=$(getparam sbox-mysqldump-dbpass)
+
+function handler () {
+  filename=$(date '+%Y-%m-%d_%H%M%S').sql.gz
+
+  mysqldump -u $DBUSER -h $DBHOST $DBNAME -p$DBPASS | \
+    gzip | \
+    aws s3 cp - s3://sbox-lambda-simple-mysqldump-s3/dump/$filename
+
+  echo $filename
 }
