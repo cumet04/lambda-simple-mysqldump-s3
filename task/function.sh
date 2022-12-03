73cda8dd244d551d61cd6eb4106ed46cc159baac
cumet04 <medalhkr@gmail.com>
2022-12-03 15:45:57 +0900

    article-step1

--- task/function.sh
+++ task/function.sh
@@ -1,9 +1,7 @@
 function handler () {
-  filename=$(date '+%Y-%m-%d_%H%M%S').sql.gz
+  EVENT_DATA=$1
+  echo "$EVENT_DATA" 1>&2;
+  RESPONSE="Echoing request: '$EVENT_DATA'"
 
-  mysqldump -u $DBUSER -h $DBHOST $DBNAME -p$DBPASS | \
-    gzip | \
-    aws s3 cp - s3://sbox-lambda-simple-mysqldump-s3/dump/$filename
-
-  echo $filename
+  echo $RESPONSE
 }
